343ee63698c410527c4fdb27d16e62afbcbd3758
Rainer Stropek <rainer@software-architects.at>
2021-04-07 19:33:03 +0200

    Extract logic in library

--- BattleshipContestFunc.GameLogic/GameClient.cs
+++ BattleshipContestFunc.GameLogic/GameClient.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("BattleshipContestFunc.Tests")]
 
 namespace BattleshipContestFunc
 {
