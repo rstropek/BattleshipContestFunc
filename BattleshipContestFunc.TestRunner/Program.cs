40a84d08d4e97a2023bab198385c5df5bb80e1f0
Rainer Stropek <rainer@software-architects.at>
2021-04-11 10:35:42 +0200

    Add test runner

--- BattleshipContestFunc.TestRunner/Program.cs
+++ BattleshipContestFunc.TestRunner/Program.cs
@@ -1,22 +1,94 @@
 ﻿using CommandLine;
+using Microsoft.Extensions.Configuration;
+using NBattleshipCodingContest.Logic;
+using Serilog;
 using System;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace BattleshipContestFunc.TestRunner
 {
+    internal record GameConfiguration(
+        PlayerHttpClientFactory PlayerFactory,
+        SinglePlayerGameFactory GameFactory,
+        IConfiguration Configuration,
+        JsonSerializerOptions JsonOptions,
+        ILogger Logger);
+
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Parser.Default.ParseArguments<TestPlayerOptions>(args)
+            return Parser.Default.ParseArguments<TestPlayerOptions, RunTournamentOptions>(args)
                 .MapResult(
                   (TestPlayerOptions options) => TestPlayer(options),
+                  (RunTournamentOptions options) => RunTournament(options),
                   errors => 1);
+        }
+
+        private static GameConfiguration GetGameConfiguration()
+        {
+            var playerClientFactory = new PlayerHttpClientFactory();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(Environment.CurrentDirectory, "appsettings.json"), true, false)
+                .AddEnvironmentVariables()
+                .Build();
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            var gameFactory = new SinglePlayerGameFactory(new RandomBoardFiller());
+
+            return new GameConfiguration(playerClientFactory, gameFactory, configuration, jsonOptions, logger);
         }
 
         private static int TestPlayer(TestPlayerOptions options)
         {
-            return 0;
+            var (playerClientFactory, _, configuration, jsonOptions, logger) = GetGameConfiguration();
+
+            var playerClient = new PlayerClient(playerClientFactory, configuration, jsonOptions);
+            try
+            {
+                logger.Information("Sending *getReady* to player");
+                playerClient.GetReady(options.WebApiUrl, options.ApiKey).Wait();
+                logger.Information("Successfully sent *getReady* to player");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error while sending *getReady* to player");
+            }
+
+            return -1;
+        }
+
+        private static int RunTournament(RunTournamentOptions options)
+        {
+            var (playerClientFactory, gameFactory, configuration, jsonOptions, logger) = GetGameConfiguration();
+
+            var playerClient = new PlayerClient(playerClientFactory, configuration, jsonOptions);
+            var gameClient = new GameClient(playerClient, gameFactory);
+            try
+            {
+                logger.Information("Starting tournament");
+                var games = gameClient.CreateTournamentGames(options.Games);
+                gameClient.PlaySimultaneousGames(options.WebApiUrl, games, 101, apiKey: options.ApiKey).Wait();
+                var stats = games.Analyze();
+                logger.Information("Successfully completed tournament. Avg. shots: {Avg}, std. dev.: {StdDev}",
+                    stats.Average, stats.StdDev);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error during tournament");
+            }
+
+            return -1;
         }
     }
 }
--- BattleshipContestFunc.TestRunner/RunTournamentOptions.cs
+++ BattleshipContestFunc.TestRunner/RunTournamentOptions.cs
@@ -3,13 +3,16 @@
 namespace BattleshipContestFunc.TestRunner
 {
 
-    [Verb("test", HelpText = "Tests a player by sending a *getReady* request.")]
-    internal class TestPlayerOptions
+    [Verb("run", HelpText = "Runs a battleship tournament")]
+    internal class RunTournamentOptions
     {
         [Option('a', "api-url", HelpText = "Web API URL", Default = "http://localhost:7071/api/")]
         public string WebApiUrl { get; set; } = "http://localhost:7071/api/";
 
         [Option('k', "api-key", HelpText = "API Key", Required = false)]
         public string? ApiKey { get; set; }
+
+        [Option('g', "games", HelpText = "Number of simultaneous games", Default = 1)]
+        public int Games { get; set; } = 1;
     }
 }
